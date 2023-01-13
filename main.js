b7c6ac16f56398450f4206f772e0d971dcd716c7
Adi <adithiyans123@gmail.com>
2023-01-13 16:05:13 +0530

    Tried to align the divs with flexbox,did not work

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const container = document.querySelector('.container');
 
 for(i=0;i<16;i++){
+    const rowcontainer = document.createElement('div');
+    rowcontainer.classList.add(`rowcont`);
+    rowcontainer.classList.add(`${i}`);
     for(j=0;j<16;j++){
         const div = document.createElement('div');
         div.classList.add(`row${i}`);
-        container.appendChild(div);
+        rowcontainer.appendChild(div);
     }
+    container.appendChild(rowcontainer);
+
 }
+
+
+const rowconts = document.querySelectorAll('.rowcont')
+rowconts.forEach((rowcont) => {
+    rowcont.style.display = 'flex';
+    rowcont.style.flex = '1';
+    rowcont.style.justifyContent = "center";
+    rowcont.style.alignItems = "spaceAround";
+
+})
