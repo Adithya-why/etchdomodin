3a3249fd76b0b45b71d479e95f09ca35fcf32382
Adi <adithiyans123@gmail.com>
2023-01-16 09:20:13 +0530

    arrange the divs using CSS grid and custom css properties

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
 const container = document.querySelector('.container');
 
+container.style.setProperty('--grid-rows',16);
+container.style.setProperty('--grid-cols',16);
+
+
 for(i=0;i<16;i++){
-    const rowcontainer = document.createElement('div');
-    rowcontainer.classList.add(`rowcont`);
-    rowcontainer.classList.add(`${i}`);
+    
     for(j=0;j<16;j++){
         const div = document.createElement('div');
-        div.classList.add(`row${i}`);
-        rowcontainer.appendChild(div);
+        div.classList.add(`row`);
+        div.classList.add(`${i}`);
+        container.appendChild(div);
     }
-    container.appendChild(rowcontainer);
+    
 
 }
 
-
-const rowconts = document.querySelectorAll('.rowcont')
-rowconts.forEach((rowcont) => {
-    rowcont.style.display = 'flex';
-    rowcont.style.flex = '1';
-    rowcont.style.justifyContent = "center";
-    rowcont.style.alignItems = "spaceAround";
-
-})
