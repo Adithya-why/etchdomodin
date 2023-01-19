8f0cfe02a133568193baaa2748584771a2639bd8
Adi <adithiyans123@gmail.com>
2023-01-19 09:05:45 +0530

    added clear button and centered the area

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const container = document.querySelector('.container');
-
-container.style.setProperty('--grid-rows',16);
-container.style.setProperty('--grid-cols',16);
+let x=16;
 
 
-for(i=0;i<16;i++){
+
+function createGrid(x){
+container.style.setProperty('--grid-rows',x);
+container.style.setProperty('--grid-cols',x);
+
+
+for(i=0;i<x;i++){
     
-    for(j=0;j<16;j++){
+    for(j=0;j<x;j++){
         const div = document.createElement('div');
         div.classList.add(`row`);
         div.classList.add(`${i}`);
@@ -24,40 +28,28 @@ elelst.forEach((ele)=>{
     })
 })
 
+}
 
+
+createGrid(x);
 
 const bt = document.querySelector('.bt');
 bt.addEventListener('click',function(){
-    let x;
     x = parseInt(prompt("Enter the Number of Rows required"));
     while(container.firstChild){
         container.removeChild(container.lastChild);
     }
 
 
-    container.style.setProperty('--grid-rows',x);
-    container.style.setProperty('--grid-cols',x);
-
-
-    for(i=0;i<x;i++){
-    
-        for(j=0;j<x;j++){
-            const div = document.createElement('div');
-            div.classList.add(`row`);
-            div.classList.add(`${i}`);
-            container.appendChild(div);
-        }
-        
-    
-    }
-
-    const elelst = document.querySelectorAll('.row');
-elelst.forEach((ele)=>{
-    ele.addEventListener('mouseover',function(){
-        ele.classList.add('hovered');
-    })
-})
+    createGrid(x)
 
 
 })
 
+
+
+const cl = document.querySelector('.cl');
+cl.addEventListener('click',function(){
+    const ds = document.querySelectorAll('.row');
+    ds.forEach((d)=>{d.classList.remove('hovered')})
+});
