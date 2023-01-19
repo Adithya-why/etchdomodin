4225ce29898ac42e9ab4fa35629238d24364c250
Adi <adithiyans123@gmail.com>
2023-01-19 08:51:19 +0530

    gave divs hiver properties and made them a fixed size

--- main.js
+++ main.js
@@ -16,3 +16,48 @@ for(i=0;i<16;i++){
 
 }
 
+
+const elelst = document.querySelectorAll('.row');
+elelst.forEach((ele)=>{
+    ele.addEventListener('mouseover',function(){
+        ele.classList.add('hovered');
+    })
+})
+
+
+
+const bt = document.querySelector('.bt');
+bt.addEventListener('click',function(){
+    let x;
+    x = parseInt(prompt("Enter the Number of Rows required"));
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+
+
+    container.style.setProperty('--grid-rows',x);
+    container.style.setProperty('--grid-cols',x);
+
+
+    for(i=0;i<x;i++){
+    
+        for(j=0;j<x;j++){
+            const div = document.createElement('div');
+            div.classList.add(`row`);
+            div.classList.add(`${i}`);
+            container.appendChild(div);
+        }
+        
+    
+    }
+
+    const elelst = document.querySelectorAll('.row');
+elelst.forEach((ele)=>{
+    ele.addEventListener('mouseover',function(){
+        ele.classList.add('hovered');
+    })
+})
+
+
+})
+
